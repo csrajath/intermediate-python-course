37a0f507bfaa4b33668032786fab29ff4c675ec4
csrajath <rajathcs.1996@gmail.com>
2021-05-20 00:27:54 -0400

    dice rolling

--- dice_roller.py
+++ dice_roller.py
@@ -1,4 +1,11 @@
+import random
 def main():
-  print('You rolled a dice')
+  roll_sum = 0
+  numberofrolls = 2
+  for i in range(0,numberofrolls):
+    rolled_num = random.randint(1,6)
+    print(f'You rolled {rolled_num}')
+    roll_sum += rolled_num
+  print(f'Total rolled value is {roll_sum}')
 if __name__ == "__main__":
   main()
